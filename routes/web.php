2b0558f553ed0781c70aadb6efe3a5272a2f012f
Chicco80 <chicco80lisa@gmail.com>
2022-06-22 20:16:55 +0200

    problema pulsante crea nuovo post

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
@@ -25,6 +25,10 @@ Route::middleware('auth')
 ->prefix('admin')
 ->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('/posts','PostController');
 });
 
 // Route::get('/home', 'HomeController@index')->name('home');
+Route::get("{any?}", function(){
+    return view('guest.home');
+})->where("any",".*");
